49698d791a0bab9838a2f5c29e4ec94831a32095
ewaszy <ewa.a.szyszkowska@gmail.com>
2021-08-19 21:59:39 -0500

    Create the New Welcome Route Complete

--- app.py
+++ app.py
@@ -4,11 +4,29 @@ from flask import Flask
 #  Create a new Flask instance called app
 app = Flask(__name__)
 
-## Create Flask Routes
-# Define the starting point
-@app.route('/')
-def hello_world():
-    return'Hello world'
+# Define the welcome route
+@app.route("/")
 
-# Run a Flask App - On Windows 
-# Enter set FLASK_APP=app.py in Anaconda Powershell
+# Add the routing information for each of the other routes
+# Create a function and a return statement with f stings as a reference to all of the other routes 
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+# Create a new route for precipitation
+@app.route("/api/v1.0/precipitation")
+
+# Create the precipitation() function
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
